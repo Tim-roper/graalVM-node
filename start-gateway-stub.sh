7648b7b2f33da639e99b3bf80f54c66699383f6b
Tim Roper <tim.roper@myob.com>
2019-09-05 11:07:08 +1000

    make artillery local

--- start-gateway-stub.sh
+++ start-gateway-stub.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker run -p 127.0.0.1:8082:8080/tcp --name gateway-stub -d realfengjia/fakeit --spec https://contracts.svc.europa-preprod.jupiter.myobdev.com/spec.json --use-example --permissive
-
+p
