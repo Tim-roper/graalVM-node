7ff23159af8d180ece23b86e2ffe4e0a17c35a1e
Tim Roper <tim.roper@myob.com>
2019-09-06 10:31:12 +1000

    additional graalVM results

--- start-sme-web-bff-graalvm.sh
+++ start-sme-web-bff-graalvm.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-graal_path="${HOME}/Downloads/graalvm_ce_19.2.0/Contents/Home/bin"
+graal_path="${HOME}/Downloads/graalvm-ce-19.2.0/Contents/Home/bin"
 sme_web_bff_path="${HOME}/work/sme-web-bff"
 
 export PATH=":${graal_path}:${PATH}"
 export NODE_ENV="dev"
 
 cd "${sme_web_bff_path}"
-node build/express.js 
+
+# node build/express.js 				# Vanilla Graal node runtime
+node --jvm --polyglot ${sme_web_bff_path}/build/express.js 		# Run on JVM
